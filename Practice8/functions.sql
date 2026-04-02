3caa65a69bd3cbf4bebce848a8e0fd95867e1e91
Dimash <dimashmirzabek22@gmail.com>
2026-04-02 18:15:29 +0500

    Practice8 - functions & procedures implemented

---
+++ Practice8/functions.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION search_contacts_by_pattern(p_pattern TEXT)
+RETURNS TABLE (id INT, name VARCHAR, phone VARCHAR)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT c.id, c.name, c.phone
+    FROM contacts c
+    WHERE c.name ILIKE '%' || p_pattern || '%'
+       OR c.phone ILIKE '%' || p_pattern || '%';
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION get_contacts_paginated(p_limit INT, p_offset INT)
+RETURNS TABLE (id INT, name VARCHAR, phone VARCHAR)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT *
+    FROM contacts
+    ORDER BY id
+    LIMIT p_limit OFFSET p_offset;
+END;
+$$ LANGUAGE plpgsql;
